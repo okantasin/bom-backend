bbd339fcea84bf3e31e50e770a03b16dab53ace7
okan <okan.tasin@hotmail.com>
2025-02-12 01:50:12 +0300

    add project structer

---
+++ src/main/java/com/java/bom/BomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.java.bom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/bom/BomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.bom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
